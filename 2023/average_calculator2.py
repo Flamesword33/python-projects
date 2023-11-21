96b0749b802a41ffa754d85bc3ba1a03a1cee3ed
Nathan Pelletier <nathantylerpelletier@gmail.com>
2023-11-21 13:58:25 -0700

    Added multi-roll for a dnd session
    said function takes a bunch of rolls and gives me how many succeeded and how many critically succeeded. Needed it for mass combat

--- 2023/average_calculator2.py
+++ 2023/average_calculator2.py
@@ -54,6 +54,23 @@ def roll(how_many = 1, face_count = 6):
         total = total + random.randint(1, face_count)
     return total
 
+def multi_roll(how_many = 1, face_count = 20, modifiyer = 0,num_to_beat = 10):
+    counter = 0
+    fails = 0
+    crits = 0
+    for x in range(1, how_many):
+        c_roll = roll(1,face_count) + modifiyer
+        if c_roll >= num_to_beat:
+            counter = counter + 1
+        if c_roll == modifiyer + 1:
+            fails = fails + 1
+        if c_roll == modifiyer + face_count:
+            crits = crits + 1
+            
+    print(counter, " hits")
+    print(fails, " fails")
+    print(crits, " crits")
+
 def advantage(face_count):
     num1 = roll(1, face_count)
     num2 = roll(1, face_count)
