034325218b996421fccbd6694afd3f1b376211be
Nathan Pelletier <nathantylerpelletier@gmail.com>
2023-12-21 20:06:48 -0700

    Got stuck after creating for loop to roll a bunch of numbers
    Thought of adding switch statement... maybe change languages then?
    Thought of if statement for each number?
    Maybe something creative like if current_roll < 10 --> ... try to get base log speeds.
    
    Finally realized that I have an array where each index is current_roll...

--- 2023/percent_chance_to_roll.py
+++ 2023/percent_chance_to_roll.py
@@ -41,6 +41,9 @@ def percent_roll(number_of_rolls = 10000):
     #             1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
     roll_tally = [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     current_roll = 0
+    for i in range(number_of_rolls):
+        current_roll = roll(1, 20)
+        #2 ways to proceed, simply enter if statement for each number from 1 to 20... im an idot
     return roll_tally
 
 main()
