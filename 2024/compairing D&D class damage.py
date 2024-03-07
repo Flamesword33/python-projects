302cba7408da956f68609b7831d3542a98b95ba1
Nathan Pelletier <nathantylerpelletier@gmail.com>
2024-03-06 22:47:15 -0700

    Create compairing D&D class damage.py

---
+++ 2024/compairing D&D class damage.py
@@ -0,0 +1,10 @@
+""" compairing D&D class damage.py
+    by Nathan Pelletier
+    Started March 6 2024
+
+    Original idea by Peter Lewis
+    Said program simulates each of D&D 5e's base classes 
+  
+"""
+
+
